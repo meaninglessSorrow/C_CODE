66c7355231a9afcce145966692624229a658d26a
Sorrow-is-meaningless <2624389515@qq.com>
2023-02-01 19:28:15 +0800

    atoi

---
+++ atoi/atoi/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int de_atio(const char* arr)
+{
+
+}
+
+int main()
+{
+	char arr1[100] = { "  +1222222223app2le3" };
+	printf("%c\n", arr1[1]);
+	int  a = atoi(arr1);
+	de_atio(arr1);
+	printf("%d\n", a);
+	return 0;
+}
