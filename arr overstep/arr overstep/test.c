1be646f202ec4da158ef8d62d303b7b12f5e3c58
Sorrow-is-meaningless <2624389515@qq.com>
2023-02-05 23:52:44 +0800

    arr overstep

---
+++ arr overstep/arr overstep/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0;i <= 12;i++)
+//	{
+//		arr[i] = 0;
+//	}
+//	return 0;
+//}
+
+//void my_strcpy(char* arr, char* arr1)
+//{
+//	while (*arr1)
+//	{
+//		*arr++ = *arr1++;
+//		/*arr++;
+//		arr1++;*/
+//	}
+//}
+//
+//int main()
+//{
+//	char arr[10] = { "hehe" };
+//	char arr1[10] = { "ha" };
+//	my_strcpy(arr, arr1);
+//	printf("%s", arr);
+//	return 0;
+//}
+
+int my_strlen()
+
+int main()
+{
+	int arr[10] = { "abcd" };
+	int n=mt_strlen(arr);
+}
