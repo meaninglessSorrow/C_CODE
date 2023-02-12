eff99bac021f83eb04e56270ceaf77feb9e4f724
Sorrow-is-meaningless <2624389515@qq.com>
2023-02-12 23:54:32 +0800

    debug2

---
+++ debug2/debug2/add.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+/*i*//*nt add(int x, int y)
+{
+	return x + y;
+}*/
+//需要在里面监视数组需要  arr,x x表示几个元素
+void test(int arr[])
+{
+
+}
---
+++ debug2/debug2/test.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include"add.h"
+
+//f10会直接跳过add函数内部
+//f11会看到add函数内部
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int ret=add(a, b);
+//	printf("%d", ret);
+//
+//	return 0;
+//}
